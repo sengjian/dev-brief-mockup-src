751aed8012ff287419d773421b7e17840380d492
SengJian <sseng@nextlabs.com>
2023-09-01 17:21:40 +0800

    Latest commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dev-brief-mockup';
+  products: any[]=[
+    { name:'Latest news at Macquarie', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+    { name:'Latest news at Macquarie', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+    { name:'Latest news at Macquarie', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+
+  ];
+  items: MenuItem[] = [
+    {
+      label: 'Loans'
+  },
+  {
+      label: 'Banking'
+  },
+  {
+      label: 'Why Macquarie?'
+  },
+  {
+      label: 'Support'
+  }
+  ]
+    responsiveOptions: any[] | undefined;
+    sidebarVisible: boolean = false;
+    constructor() {}
+
+    ngOnInit() {
+        
+
+        this.responsiveOptions = [
+            {
+                breakpoint: '1400px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '1220px',
+                numVisible: 2,
+                numScroll: 2
+            },
+            {
+                breakpoint: '1100px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { CardModule } from 'primeng/card';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { AccordionModule } from 'primeng/accordion';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MenuModule } from 'primeng/menu';
+import { SidebarModule } from 'primeng/sidebar';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +20,21 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    ToolbarModule,
+    MenubarModule,
+    ButtonModule,
+    CarouselModule,
+    CardModule,
+    ScrollTopModule,
+    AccordionModule,
+    DividerModule,
+    InputTextModule,
+    InputTextareaModule,
+    MenuModule,
+    SidebarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
